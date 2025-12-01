12183ad1c5520ba7faa35fa50f0d491598b12ba0
Gravitar64 <gravitar@web.de>
2025-12-01 23:11:41 +0100

    deutlich effizientere Lösung für Part 2

--- day01.py
+++ day01.py
@@ -6,29 +6,36 @@ def load(file):
     return [(line[0], int(line[1:])) for line in f.readlines()]
 
 
-
 def solve(p):
   position = 50
   p1 = p2 = 0
-  
+
   for richtung, anzahl in p:
     position = (position + anzahl) % 100 if richtung == 'R' else (position - anzahl) % 100
     p1 += position == 0
-  
-  position = collections.deque(range(100))
-  position.rotate(-50)
+
+  # position = collections.deque(range(100))
+  # position.rotate(-50)
+  # for richtung, anzahl in p:
+  #   if richtung == 'R':
+  #     for _ in range(anzahl):
+  #       position.rotate(1)
+  #       p2 += position[0] == 0
+  #   else:
+  #     for _ in range(anzahl):
+  #       position.rotate(-1)
+  #       p2 += position[0] == 0
+
+  position = 50
   for richtung, anzahl in p:
-    if richtung == 'R':
-      for _ in range(anzahl):
-        position.rotate(1)
-        p2 += position[0] == 0
-    else:
-      for _ in range(anzahl):
-        position.rotate(-1)
-        p2 += position[0] == 0
+    durchläufe, new_pos = divmod(position+anzahl,100) if richtung == 'R' else divmod(position-anzahl,100)
+    p2 += new_pos == 0
+    if new_pos == 0 and durchläufe > 0: durchläufe -= 1
+    if position == 0 and durchläufe < 0: durchläufe += 1
+    p2 += abs(durchläufe)
+    position = new_pos
 
-  return p1, p2  
-
+  return p1, p2
 
 
 time_start = time.perf_counter()
