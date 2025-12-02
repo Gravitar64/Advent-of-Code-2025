931eb00b4eb48774728a6af4a685db31857760ff
Gravitar64 <gravitar@web.de>
2025-12-02 08:25:19 +0100

    finale Optimierung

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
-import time, collections
+import time
 
 
 def load(file):
@@ -7,37 +7,22 @@ def load(file):
 
 
 def solve(p):
-  position = 50
   p1 = p2 = 0
 
-  for richtung, anzahl in p:
-    position = (position + anzahl) % 100 if richtung == 'R' else (position - anzahl) % 100
-    p1 += position == 0
-
-  # position = collections.deque(range(100))
-  # position.rotate(-50)
-  # for richtung, anzahl in p:
-  #   if richtung == 'R':
-  #     for _ in range(anzahl):
-  #       position.rotate(1)
-  #       p2 += position[0] == 0
-  #   else:
-  #     for _ in range(anzahl):
-  #       position.rotate(-1)
-  #       p2 += position[0] == 0
-
-  position = 50
-  for richtung, anzahl in p:
-    durchläufe, new_pos = divmod(position+anzahl,100) if richtung == 'R' else divmod(position-anzahl,100)
-    p2 += new_pos == 0
-    if new_pos == 0 and durchläufe > 0: durchläufe -= 1
-    if position == 0 and durchläufe < 0: durchläufe += 1
+  pos = 50
+  for rcht, anz in p:
+    durchläufe, neue_pos = divmod(pos + anz,100) if rcht == 'R' else divmod(pos - anz,100)
+    p1 += neue_pos == 0
+    
+    if neue_pos == 0 and durchläufe > 0: durchläufe -= 1
+    if pos == 0 and durchläufe < 0: durchläufe += 1
     p2 += abs(durchläufe)
-    position = new_pos
+    
+    pos = neue_pos
 
-  return p1, p2
+  return p1, p1 + p2
 
 
 time_start = time.perf_counter()
-print(f'Solution: {solve(load("day1.txt"))}')
+print(f'Solution: {solve(load("day01.txt"))}')
 print(f'Solved in {time.perf_counter() - time_start:.5f} Sec.')
