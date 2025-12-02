6c7607809fee749eff39a2545196dba57ee93d9a
Gravitar64 <gravitar@web.de>
2025-12-02 08:34:18 +0100

    kleine Anpassungen

--- day01.py
+++ day01.py
@@ -10,12 +10,13 @@ def solve(p):
   p1 = p2 = 0
 
   pos = 50
-  for rcht, anz in p:
-    durchläufe, neue_pos = divmod(pos + anz,100) if rcht == 'R' else divmod(pos - anz,100)
+  for richtung, anzahl in p:
+    neue_pos = pos + anzahl if richtung == 'R' else pos - anzahl
+    durchläufe, neue_pos = divmod(neue_pos,100)
     p1 += neue_pos == 0
     
     if neue_pos == 0 and durchläufe > 0: durchläufe -= 1
-    if pos == 0 and durchläufe < 0: durchläufe += 1
+    if pos == 0 and durchläufe < 0:      durchläufe += 1
     p2 += abs(durchläufe)
     
     pos = neue_pos
