14f30182c163f2a0dc752a81dd47d5bfe8569b7d
Gravitar64 <gravitar@web.de>
2025-12-07 23:39:53 +0100

    Unter 1 mx für beide Lösungen

--- day07.py
+++ day07.py
@@ -3,31 +3,26 @@ import time
 
 def load(file):
   with open(file) as f:
-    return [[c for c in zeile] for zeile in f.read().split('\n')] 
+    return [[i for i, c in enumerate(zeile) if c != '.'] for zeile in f.read().split('\n')]
 
 
 def solve(p):
   p1 = p2 = 0
-  beams = {p[0].index('S')}
-  timelines = [0] * len(p[0])
-  timelines[p[0].index('S')] = 1
-  for zeile in p[1:]:
-    new_beams = set()
-    for beam in beams:
-      if zeile[beam] == '.': 
-        new_beams.add(beam)
-        continue
-      new_beams.update({beam-1,beam+1})
-      timelines[beam-1] += timelines[beam]
-      timelines[beam+1] += timelines[beam]
-      timelines[beam] = 0
+  beams = [0] * 16
+  beams[p.pop(0)[0]] = 1
+  print(beams)
+
+  for zeile in p:
+    for splitter in zeile:
+      if not beams[splitter]: continue
       p1 += 1
-    beams = new_beams  
-  p2 += sum(timelines)
-  return p1,p2   
+      beams[splitter - 1] += beams[splitter]
+      beams[splitter + 1] += beams[splitter]
+      beams[splitter] = 0
+    #if zeile: print(beams)
+  p2 += sum(beams)
 
-
-
+  return p1, p2
 
 
 time_start = time.perf_counter()
