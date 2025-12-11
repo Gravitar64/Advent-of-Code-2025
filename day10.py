9c27fe7d7375f4ac3e4be2321e661a7eea32e085
Gravitar64 <gravitar@web.de>
2025-12-11 09:02:50 +0100

    Part1 mit Binärdarstellung

--- day10.py
+++ day10.py
@@ -3,37 +3,33 @@ import time
 
 def load(file):
   with open(file) as f:
-    return [zeile.split() for zeile in f.read().split('\n')]
+    return [[e[1:-1] for e in zeile.split()] for zeile in f]
 
 
-def zustandsänderung(state, button):
-  for n in button:
-    state[n] = '1' if state[n] == '0' else '0'
-  return state   
-
-
-def bfs(ziel, buttons):
-  start = ['0']*len(ziel)
-  queue, seen = [[start]], {tuple(start)}
+def bfs(target, buttons):
+  queue, seen = [[0]], {0}
   while queue:
-    pfad = queue.pop(0)
-    akt_zustand = pfad[-1]
-    if akt_zustand == ziel: return len(pfad)-1
+    path = queue.pop(0)
+    lights = path[-1]
+    if lights == target: return len(path) - 1
     for button in buttons:
-      neu_zustand = zustandsänderung(akt_zustand[:], button)
-      if (s:=tuple(neu_zustand)) in seen: continue
-      seen.add(s)
-      queue.append(pfad+[neu_zustand])
+      new_lights = lights ^ button
+      if new_lights in seen: continue
+      seen.add(new_lights)
+      queue.append(path + [new_lights])
 
+
+  
 
 def solve(p):
   p1 = p2 = 0
-  for i,machine in enumerate(p):
-    lights = ['1' if c == '#' else '0'for c in machine[0][1:-1]]
-    buttons = [[int(n) for n in button[1:-1].split(',')] for button in machine[1:-1]]
-    joltages = [int(n) for n in machine[-1][1:-1].split(',')]
-    p1 += bfs(lights, buttons)
-           
+  b2i = lambda x: sum(2**i for i in reversed(x))
+  
+  for lights, *buttons, joltages in p:
+    light = b2i([i for i,c in enumerate(lights) if c == '#'])
+    buttons = [b2i([int(n) for n in b.split(',')]) for b in buttons]
+    joltages = tuple(int(n) for n in joltages.split(','))
+    p1 += bfs(light, buttons)
   return p1, p2
 
 
