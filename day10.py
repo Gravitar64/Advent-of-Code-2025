ce5fe8c2fd6dd19f2702489ed661acb97f8cb385
Gravitar64 <gravitar@web.de>
2025-12-11 10:59:28 +0100

    Part1 & 2, allerdings dauert Part2 zu lang

--- day10.py
+++ day10.py
@@ -7,29 +7,54 @@ def load(file):
 
 
 def bfs(target, buttons):
-  queue, seen = [[0]], {0}
+  queue, seen = [(0,0)], {0}
   while queue:
-    path = queue.pop(0)
-    lights = path[-1]
-    if lights == target: return len(path) - 1
+    pressed, lights = queue.pop(0)
+    if lights == target: return pressed
     for button in buttons:
       new_lights = lights ^ button
       if new_lights in seen: continue
       seen.add(new_lights)
-      queue.append(path + [new_lights])
+      queue.append((pressed+1, new_lights))
 
 
+def get_joltage(state,button,anz):
+  for i in button:
+    state[i] += anz
+  return state  
+
+
+def part2(target, buttons):
+  state = [0]*len(target)
+  queue, seen = [(0,state)], {tuple(state)}
+  while queue:
+    pressed, state = queue.pop(0)
+    if state == target: return pressed
+    for anz,i in sorted([(min(target[n]-state[n] for n in b),i) for i,b in enumerate(buttons)]):
+      if anz < 0: 
+        print(anz,i)
+        continue
+      for ad in range(anz,0,-1):
+        new_state = get_joltage(state[:],buttons[i],ad)
+        #if any(a > b for a,b in zip(new_state,target)): break
+        if tuple(new_state) in seen: continue
+        seen.add(tuple(new_state))
+        queue.append((pressed+ad, new_state))
+      
   
 
 def solve(p):
   p1 = p2 = 0
-  b2i = lambda x: sum(2**i for i in reversed(x))
+  b2int = lambda x: sum(1<<i for i in reversed(x))
   
   for lights, *buttons, joltages in p:
-    light = b2i([i for i,c in enumerate(lights) if c == '#'])
-    buttons = [b2i([int(n) for n in b.split(',')]) for b in buttons]
-    joltages = tuple(int(n) for n in joltages.split(','))
-    p1 += bfs(light, buttons)
+    t1 = time.perf_counter()
+    light = b2int([i for i,c in enumerate(lights) if c == '#'])
+    buttons = [[int(n) for n in b.split(',')] for b in buttons]
+    joltages = [int(n) for n in joltages.split(',')]
+    p1 += bfs(light, [b2int(b) for b in buttons])
+    p2 += (e:=part2(joltages, buttons))
+    print(f'{e} {time.perf_counter()-t1:.2f} Sek.')
   return p1, p2
 
 
